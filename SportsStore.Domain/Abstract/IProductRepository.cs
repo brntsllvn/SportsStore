a584bdd214158adb3d7b77324fa70a83e8f593bd
= <=>
2016-04-11 20:23:44 -0700

    init

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.Domain.Abstract
+{
+    interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ SportsStore.Domain/Concrete/FakeProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SportsStore.Domain.Concrete
+{
+    class FakeProductRepository : IProductRepository
+    {
+        private static IQueryable<Product> fakeProducts = new List<Product>
+        {
+            new Product { Name = "Baseball", Price = 25 },
+            new Product { Name = "Shotgun", Price = 100 },
+            new Product { Name = "Marshmallow", Price = 72 }
+        }.AsQueryable();
+
+        public IQueryable<Product> Products
+        {
+            get { return fakeProducts; }
+        }
+    }
+}

c13c2750ba3b8f80e7685c109258c51137ac2171
Brent Sullivan <brntsllvn@gmail.com>
2016-04-12 18:38:49 -0700

    changes

---
+++ SportsStore.Domain/Concrete/SqlProductsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    class SqlProductsRepository : IProductsRepository
+    {
+        private ProductDatabase _db; 
+
+        public SqlProductsRepository()
+        {
+            
+        }
+
+        public IQueryable<Product> Products { get; }
+    }
+}
---
+++ SportsStore.Domain/ProductDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain
+{
+    public class ProductDatabase : DbContext
+    {
+        public DbSet<Product> Products { get; set; } 
+    }
+}
